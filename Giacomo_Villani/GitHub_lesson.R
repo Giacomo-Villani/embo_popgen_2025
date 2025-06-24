8b491984d19db39b774ba0d4a4dabb6e48dae1cd
Giacomo-Villani <giacomo.villani02@universitadipavia.it>
2025-06-24 13:53:44 +0200

    resolved password problem

--- Giacomo_Villani/GitHub_lesson.R
+++ Giacomo_Villani/GitHub_lesson.R
@@ -2,6 +2,6 @@
 library("usethis")
 usethis::use_git_config(user.name="villani", user.email= "giacomo.villani02@universitadipavia.it")
 usethis::create_github_token() 
-credentials::set_github_pat("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")
+credentials::set_github_pat("xxx")
 usethis::edit_r_environ()
 usethis::git_sitrep()
