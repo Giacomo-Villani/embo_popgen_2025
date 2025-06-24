c7c86700ac0f4829c905cf07c4dc421e551132c2
Giacomo-Villani <giacomo.villani02@universitadipavia.it>
2025-06-24 13:59:22 +0200

    Cleaned token and recommitted

--- Giacomo_Villani/GitHub_lesson.R
+++
@@ -1,7 +0,0 @@
-
-library("usethis")
-usethis::use_git_config(user.name="villani", user.email= "giacomo.villani02@universitadipavia.it")
-usethis::create_github_token() 
-credentials::set_github_pat("xxx")
-usethis::edit_r_environ()
-usethis::git_sitrep()
